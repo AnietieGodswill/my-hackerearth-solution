bd08a2e6d0e3205515409f205bb47532440058ce
Nishant Tiwari <dx4iot@gmail.com>
2020-04-08 00:31:38 +0530

    Create zoos.cpp

---
+++ zoos.cpp
@@ -0,0 +1,27 @@
+//zoos
+//https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/is-zoo-f6f309e7/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char x[20];
+	int z=0, o=0, i;
+	cin >> x;
+	for(i = 0; i <= 20 ; i++) {
+		if(x[i] == 'z'){
+			z++;
+		}
+		else if(x[i] == 'o') {
+			o++;			
+		}
+	
+	}
+	if(2*z == o) {
+		cout<<"Yes";
+	}
+	else{
+		cout<<"No";
+	}
+		
+	return 0;
+}
