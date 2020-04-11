d79b2d93ab50152cce072a359c86963d64e0527c
Nishant Tiwari <dx4iot@gmail.com>
2020-04-11 23:29:17 +0530

    Update factorials.py

--- factorials.py
+++ factorials.py
@@ -1,5 +1,5 @@
-//Factorial! 
-//https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/find-factorial/
+#Factorial! 
+#https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/find-factorial/
 def fact():
     f =1
     for i in range(1,n+1): 
